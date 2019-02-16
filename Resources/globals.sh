d010bca49b0146ba4b360246dfb2e0dd1262be93
Campbell Guise <cam@guise.co.nz>
2019-02-16 14:52:17 +1300

    Create globals.sh
    Created file to manage all global static variables as well as global placeholder variable names which may get used in various functions.

---
+++ Resources/globals.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+## Global Variables ##
+# User custom variables
+CONNECTION_MAX_TIMEOUT=30 # Seconds before the connection attempt will timeout
+
+# Static variables
+HEADER_CONTENT_TYPE="Content-Type: text/xml"
+HEADER_ACCEPT="Accept: text/xml"
+CONFIG_REQUIRED_ITEMS=("JAMF_AUTH_KEY" "JAMF_URL")
+
+# Placeholder variable names
+VERBOSE=false
+DRY_RUN=false
+OUTPUT_FILE=""
+CONFIG_FILE=""
+JAMF_AUTH_KEY=""
+JAMF_URL=""
