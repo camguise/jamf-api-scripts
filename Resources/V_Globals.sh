4c6756ad3fc46339686e33e0e5eb37ecc95ff3f6
Campbell Guise <cam@guise.co.nz>
2019-02-17 20:06:43 +1300

    Removed sheebang from V_Globals

--- Resources/V_Globals.sh
+++ Resources/V_Globals.sh
@@ -6,7 +6,6 @@ CONNECTION_MAX_TIMEOUT=30 # Seconds before the connection attempt will timeout
 HEADER_CONTENT_TYPE="Content-Type: text/xml"
 HEADER_ACCEPT="Accept: text/xml"
 CONFIG_REQUIRED_ITEMS=("JAMF_AUTH_KEY" "JAMF_URL")
-set -e # Ensure that script will exit if any of the functions produce a non-zero exit code
 
 # Placeholder variable names
 VERBOSE=false
