94e568cc59e067685d00fdcee911f1d480b09d24
Campbell Guise <cam@guise.co.nz>
2019-02-24 16:52:56 +1300

    Added bootstrap back into dev branch

---
+++ JamfBootstrap.sh
@@ -0,0 +1,115 @@
+#!/bin/bash
+### Name: JamfBootstrap.sh
+### Description: This script performs initial configuration for a new Jamf instance via
+###   the API. This script was created for use within my organisation (Cyclone Computer
+###   Company Ltd) you may need to modify it for your own environment.
+### Created by: Campbell Guise - cam@guise.co.nz
+### Created: 2019-02-23
+
+COMPANY_NAME="cyclone" # Used to create api user
+
+## Source External Files ##
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )" # parent folder of script
+for resource in "${DIR}"/Resources/*.sh; do source "${resource}"; done
+
+## Command line options/arguments ##
+
+# Display help text to describe available command-line options
+helpText () {
+	echo "Usage: $(basename "$0") [options] -o /OUTPUT/Path/file.cfg
+	
+-- Apply base settings to an existing Jamf Pro instance and produce a config file for future use
+e.g. $(basename "$0") -v -o ~/Downloads/customer.cfg
+	
+options:
+    -h                show this help text
+    -o [file path]    output file for config
+    -v                verbose output"
+}
+
+while getopts ":o:vh" opt; do
+	case $opt in
+		o)
+			OUTPUT_FILE=$(realPath "$OPTARG")
+			;;
+		v)
+			VERBOSE=true
+			;;
+		h)
+			helpText
+			exit 0
+			;;
+		\?)
+			echo "Invalid option: -$OPTARG" >&2
+			exit 1
+			;;
+		:)
+			echo "Option -$OPTARG requires an argument." >&2
+			exit 1
+			;;
+	esac
+done
+
+## MAIN SCRIPT ##
+
+if [[ -z "${OUTPUT_FILE}" ]]; then
+	echo "Error: You must specify an output file (-o)" >&2
+	echo ""
+	helpText
+	exit 1
+fi
+
+jamfAddress=$(getUserInputMatchingRegex "Jamf Pro server address" \
+	"${JAMF_URL_REGEX}" "You must specify a valid server URL, including 'https://'")
+jamfAdminUser=$(getUserInputMatchingRegex "Jamf admin username" "^([a-z]|[0-9]|-)+$" \
+	"Username must contain only lowercase letters, numbers or a hyphen")
+jamfAdminPassword=$(getUserInputMatchingRegex "Jamf admin password" "${REGEX_ANY}" "" true)
+echo -e "\n"
+
+jamfAdminKey=$(echo -n "${jamfAdminUser}:${jamfAdminPassword}" | base64)
+
+JAMF_AUTH_KEY="${jamfAdminKey}"
+JAMF_URL="${jamfAddress}"
+
+jamfApiUser="${COMPANY_NAME}-api"
+jamfApiPassword=$(openssl rand -base64 32 | tr -cd '[a-zA-Z0-9]._-')
+
+xmlData="
+<account>
+  <name>${jamfApiUser}</name>
+  <directory_user>false</directory_user>
+  <full_name>API User</full_name>
+  <email/>
+  <email_address/>
+  <enabled>Enabled</enabled>
+  <force_password_change>false</force_password_change>
+  <access_level>Full Access</access_level>
+  <privilege_set>Custom</privilege_set>
+  <privileges>
+    <jss_objects>
+      <privilege>Read Categories</privilege>
+    </jss_objects>
+    <jss_settings/>
+    <jss_actions/>
+    <recon/>
+    <casper_admin/>
+    <casper_remote/>
+    <casper_imaging/>
+  </privileges>
+</account>
+"
+
+httpPost "/JSSResource/accounts" "${xmlData}"
+
+createConfig "${jamfAddress}" "${jamfApiUser}" "${jamfApiPassword}"
+apiUserXml=$(httpGet "/JSSResource/accounts/username/${jamfApiUser}")
+apiUserID=$(getXPathValue "/account/id" "${apiUserXml}")
+echo "####################################################################################"
+echo "# You need to go to the Jamf Pro server and set the password for your new API user #"
+echo "# URL: ${JAMF_URL}/accounts.html?id=${apiUserID}&o=u                               #"
+echo "# User: ${jamfApiUser}                                                             #"
+echo "# Password: ${jamfApiPassword}                                                     #"
+echo "# -------------------------------------------------------------------------------- #"
+echo "# You can then test your config file:                                              #"
+echo "# $ ./testConfigFile.sh -c ${OUTPUT_FILE}                                          #"
+echo "####################################################################################"
