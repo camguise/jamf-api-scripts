5fcbd93bfabf9608041cedb9a61536be14e77ad8
Campbell Guise <cam@guise.co.nz>
2019-02-24 21:21:29 +1300

    Created CSV validation fuction

--- Resources/F_CSVConfig.sh
+++ Resources/F_CSVConfig.sh
@@ -5,3 +5,52 @@
 ### Created by: Campbell Guise - cam@guise.co.nz
 ### Created: 2019-02-24
 
+# -------------------------------------
+# Checks a CSV to ensure it matches the requirements for jamf-api-scripts
+# Globals:
+#   NONE
+# Arguments:
+#   csvFile - The CSV file to be validated
+# Returns:
+#   true or false depending on if the csv is valid
+# -------------------------------------
+function validateCSV () {
+	local csvFile="$1"
+	
+	local err=false
+	
+	if [[ ! -f "${csvFile}" ]]; then
+		echo "Error: No CSV file found at ${csvFile}" >&2
+		false
+		return
+	fi
+	
+	local csvRequiredHeaders=("App Name" "Folder Name" "iTunes URL" "%groups_start%" "%groups_end%")
+	
+	IFS= read -r headerRow < "${csvFile}"
+	
+	IFS=, read -ra headerFields <<<"${headerRow}"
+	for header in "${csvRequiredHeaders[@]}"; do
+		if ! arrayContains "${header}" "${headerFields[@]}"; then
+			err=true
+			echo "Error: Required CSV header '${header}' not found" >&2
+		fi
+	done
+	
+	groupsLine=$(echo "${headerRow}" | awk -F",%groups_start%,|,NULL,|,%groups_end%," '{ print $2; }')
+	
+	buildings=()
+	IFS=, read -ra fields <<<"${groupsLine}"
+	for field in "${fields[@]}"; do
+		buildings+=("${field}")
+	done
+	
+	buildingsCount="${#buildings[@]}"
+	
+	if [[ ${buildingsCount} -lt 1 ]]; then
+		err=true
+		echo "Error: No device groups found in CSV" >&2
+	fi
+	
+	! $err
+}
