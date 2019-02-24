944c1832156d95d90a6142652c733471857d08a0
Campbell Guise <cam@guise.co.nz>
2019-02-24 20:33:43 +1300

    Created file for CSV processing functions

---
+++ Resources/F_CSVConfig.sh
@@ -0,0 +1,7 @@
+### Name: F_CSVConfig.sh
+### Description: Functions which process CSV input. There is a template CSV file in the
+###   Templates folder. The CSV is taken from a spreadsheet containing the complex
+###   formulas for app distribution.
+### Created by: Campbell Guise - cam@guise.co.nz
+### Created: 2019-02-24
+
